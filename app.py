7c709bdba71431ec9f647812c27fcf5a39c0ad6e
Pranav Senthilvel <pranav.senthilvel@gmail.com>
2020-04-20 23:13:34 -0400

    work on navbar and title icon

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
+from flask_session.__init__ import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -10,6 +10,8 @@ from datetime import datetime
 
 from helpers import apology, login_required, lookup, usd
 
+app = Flask(__name__)
+
 @app.route("/")
-def index:
+def index():
     return render_template("index.html")
