e38036ecb81c854558dc2bb2e681e9120d7f126b
Pranav Senthilvel <pranav.senthilvel@gmail.com>
2020-04-21 23:48:05 -0400

    add home page redirect and framework for login system

--- app.py
+++ app.py
@@ -12,6 +12,39 @@ from helpers import apology, login_required, lookup, usd
 
 app = Flask(__name__)
 
+# ensures templates aren't reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# ensures responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route("/")
+@login_required
 def index():
-    return render_template("index.html")
+    #TODO
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    # TODO
+
+@app.route("/register")
+def register():
+    # TODO
